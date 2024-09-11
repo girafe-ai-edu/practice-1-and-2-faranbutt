b10fd852eb55ce80eee472b631310693cddbb4e1
Faran Butt <faranbutt789@gmail.com>
2024-09-12 00:01:14 +0500

    Solved practice 1 and practice 2

--- practice_2/task_3.py
+++ practice_2/task_3.py
@@ -3,9 +3,20 @@
 Write a program to calculate the body mass index (BMI) of a person. The user must enter their height and weight, after which you use one of the formulas below to determine the index.
 BMI = weight/height**2 
 """
-weight = input()
-height = input()
+weight = float(input('Enter your weight in kg: '))
+height = float(input('Enter your height in meters: '))
 
 
 #code here
+BMI = weight/(height**2)
+print(f'Your BMI is {BMI:.2f}')
+
+if BMI < 18.5:
+    print('Underweight')
+elif BMI >= 18.5 and BMI < 25:
+    print('Normal weight')
+elif BMI >= 25 and BMI < 30:
+    print('Overweight')
+else:
+    print('Obesity')
 
--- practice_2/task_4.py
+++ practice_2/task_4.py
@@ -4,3 +4,14 @@ Develop a program that asks the user for an integer 4-digit number and calculate
 3 + 1 + 4 + 1 = 9
 
 """
+total = 0
+while True: 
+    number = input('Enter a 4-digit number: ')
+    if len(number) != 4:
+        print('Please enter a 4-digit number')
+    else:
+        break
+
+for num in number:
+    total += int(num)
+print(f'The sum of the digits is {total}')
